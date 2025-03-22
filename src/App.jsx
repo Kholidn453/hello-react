df2e0168a570c8a0f7970acd589f9c6e4e8314ee
Kholidn453 <Kholidnasrullah06@gmail.com>
2025-03-22 15:34:13 +0700

    Pertemuan 2

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import libraries
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+// import component
+
+
+function App (){
+  return (
+    <div>
+    <h1>Hello React</h1>
+    <Header />
+    <Footer />
+    <Header />
+    </div>
+  );
+}
+// Export default
+export default App;
+
